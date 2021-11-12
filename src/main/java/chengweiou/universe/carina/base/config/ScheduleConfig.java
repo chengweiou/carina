298a3486042e36223cd7a447414473b7421e72b2
chengwei ou <chengweiou@chengweis-MacBook-Pro.local>
2021-11-11 22:49:23 -0800

    add config

---
+++ src/main/java/chengweiou/universe/carina/base/config/ScheduleConfig.java
@@ -0,0 +1,10 @@
+package chengweiou.universe.carina.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+// todo tip: 使用时开启
+@Configuration
+// @EnableScheduling
+public class ScheduleConfig {
+}
